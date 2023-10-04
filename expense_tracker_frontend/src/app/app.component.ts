24c967a96b6f1c8ab265ada72b109da2a9e29b20
Shakiful <shakifulkhan@gmail.com>
2023-10-04 15:10:30 +0600

    sidebar and topbar complete without icons

--- expense_tracker_frontend/src/app/app.component.ts
+++ expense_tracker_frontend/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'expense_tracker';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    matIconRegistry
+      .addSvgIcon(
+        'angular',
+        domSanitizer.bypassSecurityTrustResourceUrl(
+          'https://0t2.github.io/angular-material-notes/svg/angular.svg'
+        )
+      )
+      .addSvgIconInNamespace(
+        'custom-svg',
+        'angular',
+        domSanitizer.bypassSecurityTrustResourceUrl(
+          'https://0t2.github.io/angular-material-notes/svg/angular_solidBlack.svg'
+        )
+      )
+      .addSvgIconSetInNamespace(
+        'core',
+        domSanitizer.bypassSecurityTrustResourceUrl(
+          'https://0t2.github.io/angular-material-notes/svg/core-icon-set.svg'
+        )
+      )
+      .registerFontClassAlias('fontawesome', 'fa');
+  }
 }
--- expense_tracker_frontend/src/app/dashboard/dashboard.module.ts
+++ expense_tracker_frontend/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule } from '@angular/router';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [RouterModule, DashboardRoutingModule],
+  imports: [RouterModule, DashboardRoutingModule, MatIconModule],
 })
 export class DashboardModule {}
